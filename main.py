f21b6aeda3c0d6bd6e69a366785eb8ad2785afac
Chris Warrilow <meth0d@comcast.net>
2021-02-17 22:52:50 +0000

    finished

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ split= int(input('How many total people split the bill? '))
 
 percentTotal = tip / 100 * totalCost + totalCost
 tipSplit = round(percentTotal / split, 2)
-#total = round(tipSplit, 4)
+tipSplit = '{:.2f}'.format(tipSplit)
+
 
 
 print(f'Each person should pay: ${tipSplit}')
